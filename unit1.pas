7c82fe50455bcbc8fabe5775f966e311f09bca84
Nani <hamza.benismail.6@gmail.com>
2024-05-08 18:07:07 +0100

    first commit

---
+++ unit1.pas
@@ -0,0 +1,397 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Unit2;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
+    Button18: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Memo1: TMemo;
+    ToggleBox1: TToggleBox;
+    procedure Button10Click(Sender: TObject);
+    procedure Button11Click(Sender: TObject);
+    procedure Button12Click(Sender: TObject);
+    procedure Button13Click(Sender: TObject);
+    procedure Button14Click(Sender: TObject);
+    procedure Button15Click(Sender: TObject);
+    procedure Button16Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
+    procedure Button18Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
+    procedure ToggleBox1Change(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+procedure Delay(dt: DWORD);
+var
+  tc : DWORD;
+begin
+  tc := GetTickCount;
+  while (GetTickCount < tc + dt) and (not Application.Terminated) do
+    Application.ProcessMessages;
+end;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  {
+  //button toggle
+  if togglebox1.Caption='One Line Mode On' then
+  begin
+   memo1.Lines[0]:=memo1.Lines[0]+button1.Caption
+  end;
+  //button toggle
+  }
+
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button1.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button1.Caption;
+end;
+
+procedure TForm1.Button18Click(Sender: TObject);
+begin
+  {
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+  }
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button18.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button18.Caption;
+end;
+
+procedure TForm1.Button10Click(Sender: TObject);
+begin
+ memo1.Clear;
+  memo1.Lines[0]:='0';
+end;
+
+procedure TForm1.Button11Click(Sender: TObject);
+var
+  ch:string;
+begin
+  if memo1.Lines[2] <> '' then
+  begin
+  ch:=memo1.lines[2];
+  delete(ch,length(ch),1);
+  memo1.lines[2]:=ch;
+  end
+  else if memo1.Lines[1] <> '' then
+  begin
+  ch:=memo1.lines[1];
+  delete(ch,length(ch),1);
+  memo1.lines[1]:=ch;
+  end
+  else
+  // memo1.Lines[0] <> '' then
+  begin
+  ch:=memo1.lines[0];
+  delete(ch,length(ch),1);
+  memo1.lines[0]:=ch;
+  end ;
+
+end;
+
+procedure TForm1.Button12Click(Sender: TObject);
+begin
+ if memo1.Lines[0] <> '' then
+  memo1.Lines[1]:=button12.Caption;
+end;
+
+procedure TForm1.Button13Click(Sender: TObject);
+begin
+ if memo1.Lines[0] <> '' then
+  memo1.Lines[1]:=button13.Caption;
+end;
+
+procedure TForm1.Button14Click(Sender: TObject);
+begin
+ if memo1.Lines[0] <> '' then
+  memo1.Lines[1]:=button14.Caption;
+end;
+
+procedure TForm1.Button15Click(Sender: TObject);
+begin
+ if memo1.Lines[0] <> '' then
+  memo1.Lines[1]:=button15.Caption;
+end;
+
+procedure TForm1.Button16Click(Sender: TObject);
+begin
+ if memo1.Lines[0] <> '' then
+  memo1.Lines[1]:=button16.Caption;
+end;
+
+procedure TForm1.Button17Click(Sender: TObject);
+var
+  ch,x:string;
+  f:text;
+begin
+  assignfile(f,'default.txt');
+ reset(f);
+ read(f,x);
+ closefile(f);
+
+ if (memo1.lines[0]=x)and(memo1.lines[1]='')and(memo1.lines[2]='') then
+ begin
+ form2.ShowModal;
+ end;
+
+
+ if memo1.Lines[0]='00000' then
+ begin
+ memo1.Clear;
+ memo1.Lines.add('0');
+ assignfile(f,'default.txt');
+ rewrite(f);
+ delay(5000);
+ write(f,memo1.Lines[0]);
+ closefile(f);
+ end;
+
+
+
+ if memo1.lines[1]='+' then
+ begin
+ str(strtoint(memo1.lines[0])+strtoint(memo1.lines[2]),ch);
+
+  //memo1.font.color:=clscrollbar;
+  memo1.lines[3]:=ch+'                                                             ';
+ end;
+
+  if memo1.lines[1]='-' then
+ begin
+ str(strtoint(memo1.lines[0])-strtoint(memo1.lines[2]),ch);
+  memo1.lines[3]:=ch+'                                                             ';
+ end;
+
+  if memo1.lines[1]='x' then
+ begin
+ str(strtoint(memo1.lines[0])*strtoint(memo1.lines[2]),ch);
+  memo1.lines[3]:=ch+'                                                             ';
+ end;
+
+  // div 0 null
+  if (memo1.lines[1]='/')then
+ begin
+ str(strtoint(memo1.lines[0]) / strtoint(memo1.lines[2]),ch);
+  memo1.lines[3]:=ch+'                                                             ';
+ end;
+
+  if memo1.lines[1]='%' then
+ begin
+ str(strtoint(memo1.lines[0]) mod strtoint(memo1.lines[2]),ch);
+  memo1.lines[3]:=ch+'                                                             ';
+ end;
+
+ memo1.ScrollBy(1,2);
+ {
+ case memo1.lines[1] of
+  '+':memo1.lines[3]:=strtoint(memo1.lines[0])+strtoint(memo1.lines[2]);
+  '-':memo1.lines[3]:=strtoint(memo1.lines[0])+strtoint(memo1.lines[2]);
+  'x':memo1.lines[3]:=strtoint(memo1.lines[0])+strtoint(memo1.lines[2]);
+  '/':memo1.lines[3]:=strtoint(memo1.lines[0])+strtoint(memo1.lines[2]);
+  '%':memo1.lines[3]:=strtoint(memo1.lines[0])+strtoint(memo1.lines[2]);
+ end;
+ }
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button2.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button2.Caption;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button3.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button3.Caption;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+ if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button4.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button4.Caption;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button5.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button5.Caption;
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+   begin
+   memo1.clear;
+   end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button6.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button6.Caption;
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+ if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+ if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button7.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button7.Caption;
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+begin
+  if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+  memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button8.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button8.Caption;
+end;
+
+procedure TForm1.Button9Click(Sender: TObject);
+begin
+  if (memo1.Lines[0]='0') or (memo1.Lines[0]='') then
+  begin
+ memo1.clear;
+  end;
+
+  if memo1.Lines[1]='' then
+  memo1.Lines[0]:=memo1.Lines[0]+button9.Caption
+ else
+ memo1.lines[2]:=memo1.Lines[2]+button9.Caption;
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+var
+  f:text;
+begin
+  memo1.Clear;
+  memo1.Lines.Add('0');
+  if not (fileexists('default.txt')) then
+  begin
+  assignfile(f,'default.txt');
+  rewrite(f);
+  write(f,'123');
+  closefile(f);
+
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Memo1Change(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.ToggleBox1Change(Sender: TObject);
+begin
+  {
+  if togglebox1.caption='One Line Mode Off' then
+  togglebox1.Caption:='One Line Mode On'
+  else
+   togglebox1.Caption:='One Line Mode Off'
+   }
+end;
+
+end.
+
---
+++ unit2.pas
@@ -0,0 +1,184 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  EditBtn, ExtCtrls, ShellCtrls,dos,crt;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    FileNameEdit1: TFileNameEdit;
+    Label1: TLabel;
+    ListBox1: TListBox;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FileNameEdit1Change(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDropFiles(Sender: TObject; const FileNames: array of string);
+    procedure ListBox1Click(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
+    procedure Memo1DragDrop(Sender, Source: TObject; X, Y: Integer);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+
+{$R *.lfm}
+
+{ TForm2 }
+
+procedure TForm2.FormActivate(Sender: TObject);
+begin
+  memo1.Clear;
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.FormDropFiles(Sender: TObject; const FileNames: array of string);
+var
+  i:integer;
+begin
+  for i:=low(filenames) to high(filenames) do
+  begin
+  memo1.Append(fileNames[i]);
+  end;
+end;
+
+procedure TForm2.ListBox1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Memo1Change(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Memo1DragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+
+end;
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  i:integer;
+  //f:file of char;
+  f:textfile;
+  j:char;
+begin
+
+  RenameFile('C:\Users\Naniii\Desktop\english project\testetste.txt','C:\Users\Naniii\Desktop\english project\NewDir\testetste.txt');
+
+  {
+  if DirectoryExists('NewDir') then
+     begin
+       for i:=0 to memo1.Lines.Count do
+       begin
+       RenameFile(memo1.lines[i],'C:\Users\Naniii\Desktop\english project\NewDir\'+ExtractFileName(memo1.Lines[i]));
+       showmessage('done1');
+        //listbox1.items.add(memo1.lines[i]);
+       end;
+     end
+  else
+  begin
+  CreateDir ('NewDir');
+   for i:=0 to memo1.Lines.Count do
+       begin
+   RenameFile(memo1.lines[i],'C:\Users\Naniii\Desktop\english project\NewDir\'+ExtractFileName(memo1.Lines[i]));
+   showmessage('done2');
+   end;
+   //listbox1.items.add(memo1.lines[i]);
+  end;
+    }
+
+  {
+  //assignfile(f,'load_file.dat');
+  if not(FileExists('testetste.txt')) then
+    begin
+  assignfile(f,'testetste.txt');
+  rewrite(f);
+
+   for i :=0 to memo1.lines.count do
+  begin
+    for j in memo1.Lines[i]do
+    begin
+    writeln(f,j);
+    end;
+  end;
+end
+  else
+
+  begin
+  assignfile(f,'testetste.txt');
+  append(f);
+
+      for i :=0 to memo1.lines.count do
+  begin
+    for j in memo1.Lines[i]do
+    begin
+    write(f,j);
+    end;
+  end;
+
+  end;
+
+  closefile(f);
+  }
+
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+begin
+  {
+   if listbox1.Count>0 then
+     begin
+     RenameFile('C:\Users\Naniii\Desktop\english project\NewDir\'+ExtractFileName(listbox1.GetSelectedText),listbox1.GetSelectedText);
+  //showmessage(listbox1.GetSelectedText);
+     end;
+     }
+
+  {
+  s:='';
+  assignfile(f,'testetste.txt');
+  reset(f);
+  for i :=1 to filesize(f) do
+begin
+read(f,c);
+s:=s+c;
+//writeln(c);
+end;
+  showmessage(s);
+
+  closefile(f);
+
+ //showmessage(Paramstr(0));
+ }
+end;
+
+procedure TForm2.FileNameEdit1Change(Sender: TObject);
+begin
+  memo1.Append(filenameedit1.Text);
+end;
+
+end.
+
